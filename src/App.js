210bc6d5ab2c966b27a96288bcb4a29c68ecd4d4
EdwardHan <krcompliance@gmail.com>
2019-03-09 20:59:07 +0800

    Create customer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customers from './components/Customers'
 
+const customer = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': 'Tony Han',
+  'birthday': '691114',
+  'gender': 'Male',
+  'job': 'Developer'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': 'Elena Han',
+  'birthday': '701114',
+  'gender': 'Female',
+  'job': 'Student'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': 'Edward Han',
+  'birthday': '791114',
+  'gender': 'Female',
+  'job': 'HouseWife'
+}]
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="gray-background">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Let's develop management project using react.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        {
+          customer.map(c => {
+            return ( 
+                <Customers
+                key = {c.id}
+                id = {c.id}
+                name = {c.name}
+                image = {c.image}
+                birthday = {c.birthday}
+                gender = {c.gender}
+                job = {c.job}
+                />
+            )
+          })
+        }
       </div>
-    );
+    )
   }
 }
 
